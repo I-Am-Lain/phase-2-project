3c2f8318013ae21b6198854f9f5a3ef31b4e02f7
I-Am-Lain <vetter.william@gmail.com>
2020-10-21 22:20:51 -0500

    Accepted all changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'rest-client'
 require 'json'
-<<<<<<< HEAD
-
-# REFER TO API DOC FOR ESCAPING ''
-response_string = RestClient.get("https://opentdb.com/api.php?amount=50&type=multiple")
-=======
 require 'byebug'
 
 response_string = RestClient.get("https://opentdb.com/api.php?amount=50&type=multiple") 
->>>>>>> Justin
 response_hash = JSON.parse(response_string)
 question_data = response_hash["results"]
 
 question_data.each do |que|
     Category.find_or_create_by(name: que["category"])
-<<<<<<< HEAD
-end
-
-# create new question in db as user creates a question
-# join table to assoc question
-# user authe    store user id in session, create UserQuestion with current session[:user_id] and the question
-
-question_data.each do |que|
-    Question.create(content: que["question"],
-=======
     
 end
 
@@ -38,7 +22,6 @@ end
 
 question_data.each do |que|
     Question.create(content: que["question"].encode("UTF-8"),
->>>>>>> Justin
     difficulty: que["difficulty"],
     correct: que["correct_answer"],
     inc1: que["incorrect_answers"][0],
@@ -49,13 +32,8 @@ end
 
 
 
-<<<<<<< HEAD
-User.create(name: "William", picture: "https://i.pinimg.com/originals/fa/44/fd/fa44fda4399559fa9dc418c7bb390457.png", bio: "blehhhhh")
-User.create(name: "Justin", picture: "https://momandmore.com/wp-content/uploads/2014/09/morton-salt-girl-logo.png", bio: "yahoooooooooooooooooooooo")
-=======
 # User.create(name: "William", picture: "https://i.pinimg.com/originals/fa/44/fd/fa44fda4399559fa9dc418c7bb390457.png", bio: "blehhhhh")
 # User.create(name: "Justin", picture: "https://momandmore.com/wp-content/uploads/2014/09/morton-salt-girl-logo.png", bio: "yahoooooooooooooooooooooo")
->>>>>>> Justin
 
 
 UserQuestion.create(user_id: 1, question_id: 1)
